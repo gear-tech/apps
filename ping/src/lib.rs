7889e5727b749f0fc1c63557d2d01442a27f84fd
Shamil <shamil@shamil.ru>
2022-02-21 11:45:53 +0300

    Update `demo-ping` to use new builder and test framework

--- ping/src/lib.rs
+++ ping/src/lib.rs
@@ -21,5 +21,23 @@ pub unsafe extern "C" fn handle() {
     }
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn init() {}
+#[cfg(test)]
+mod tests {
+    extern crate std;
+
+    use gtest::{Log, Program, System};
+
+    #[test]
+    fn it_works() {
+        let system = System::new();
+        system.init_logger();
+
+        let program = Program::current(&system);
+
+        let res = program.send_bytes(42, "INIT");
+        assert!(res.log().is_empty());
+
+        let res = program.send_bytes(42, "PING");
+        assert!(res.contains(&Log::builder().source(1).dest(42).payload_bytes("PONG")));
+    }
+}
