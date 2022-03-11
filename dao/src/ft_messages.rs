af4b91a5a439d40cad45654a4ebf55f46f3fe485
Louisa <30317856+LouiseMedova@users.noreply.github.com>
2022-03-11 14:21:48 +0300

    Updated DAO according to the changes in fungible token (#28)
    * changed fungible token
    
    * updated DAO
    
    * Add an empty line at the end of file
    
    * Add whitespace after curly bracket
    
    * fixed according to comments
    
    * fixed Makefile
    
    Co-authored-by: Shamil <66209982+shamilsan@users.noreply.github.com>

---
+++ dao/src/ft_messages.rs
@@ -0,0 +1,42 @@
+use ft_io::*;
+use gstd::{msg, ActorId};
+
+pub async fn transfer_from_tokens(token_id: &ActorId, from: &ActorId, to: &ActorId, amount: u128) {
+    let _transfer_response: Event = msg::send_and_wait_for_reply(
+        *token_id,
+        Action::Transfer {
+            from: *from,
+            to: *to,
+            amount,
+        },
+        0,
+    )
+    .await
+    .expect("Error in transfer");
+}
+
+pub async fn transfer_tokens(token_id: &ActorId, from: &ActorId, to: &ActorId, amount: u128) {
+    let _transfer_response: Event = msg::send_and_wait_for_reply(
+        *token_id,
+        Action::Transfer {
+            from: *from,
+            to: *to,
+            amount,
+        },
+        0,
+    )
+    .await
+    .expect("Error in transfer");
+}
+
+pub async fn balance(token_id: &ActorId, account: &ActorId) -> u128 {
+    let balance_response: Event =
+        msg::send_and_wait_for_reply(*token_id, Action::BalanceOf(*account), 0)
+            .await
+            .expect("Error in balance");
+    if let Event::Balance(balance_response) = balance_response {
+        balance_response
+    } else {
+        0
+    }
+}
