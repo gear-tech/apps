b2586ae74ffa018cb3fa0552941317b8f4e2b391
puzzle-rusher <puzzlerusher@gmail.com>
2022-04-14 06:21:37 +0400

    tests and contract rework

--- dutch-auction/io/src/lib.rs
+++ dutch-auction/io/src/lib.rs
@@ -8,6 +8,7 @@ use scale_info::TypeInfo;
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub enum Action {
     Buy,
+    Create(CreateConfig),
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
@@ -17,15 +18,21 @@ pub enum Event {
         to: ActorId,
         token_id: U256,
     },
+    AuctionStarted {
+        token_owner: ActorId,
+        price: U256,
+        token_id: U256,
+    },
 }
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
-pub struct InitConfig {
+pub struct InitConfig { }
+
+#[derive(Debug, Encode, Decode, TypeInfo)]
+pub struct CreateConfig {
+    pub nft_contract_actor_id: ActorId,
+    pub token_owner: ActorId,
+    pub token_id: U256,
     pub starting_price: U256,
     pub discount_rate: U256,
-    pub token_id: U256,
-
-    pub name: String,
-    pub symbol: String,
-    pub base_uri: String,
 }
--- dutch-auction/src/lib.rs
+++ dutch-auction/src/lib.rs
@@ -2,25 +2,23 @@
 #![feature(const_btree_new)]
 
 use codec::Encode;
-use gstd::{debug, msg, prelude::*, exec::block_timestamp, ActorId};
+use gstd::{msg, prelude::*, exec::block_timestamp, ActorId};
 use primitive_types::U256;
+use nft_example_io;
 
 pub mod state;
 pub use state::{State, StateReply};
 
-pub use auction_io::{Action, Event, InitConfig};
-
-use non_fungible_token::base::NonFungibleTokenBase;
-use non_fungible_token::NonFungibleToken;
+pub use auction_io::{Action, Event, InitConfig, CreateConfig};
 
 const ZERO_ID: ActorId = ActorId::new([0u8; 32]);
 const DURATION: u64 = 7 * 24 * 60 * 60 * 1000;
 
 #[derive(Debug)]
 pub struct NFT {
-    pub token: NonFungibleToken,
     pub token_id: U256,
     pub owner: ActorId,
+    pub contract_id: ActorId,
 }
 
 #[derive(Debug)]
@@ -28,28 +26,28 @@ pub struct Auction {
     pub nft: NFT,
     pub starting_price: U256,
     pub discount_rate: U256,
-    pub already_bought: bool,
+    pub is_using: bool,
     pub start_at: u64,
     pub expires_at: u64,
 }
 
 static mut CONTRACT: Auction = Auction {
     nft: NFT {
-        token: NonFungibleToken::new(),
         token_id: U256::zero(),
         owner: ZERO_ID,
+        contract_id: ZERO_ID,
     },
     starting_price: U256::zero(),
     discount_rate: U256::zero(),
-    already_bought: false,
+    is_using: false,
     start_at: 0,
     expires_at: 0,
 };
 
 impl Auction {
     fn buy(&mut self) {
-        if self.already_bought {
-            panic!("already bought");
+        if !self.is_using {
+            panic!("already bought or auction expired");
         }
 
         if block_timestamp() >= self.expires_at {
@@ -62,14 +60,16 @@ impl Auction {
             panic!("value < price");
         }
 
-        self.already_bought = true;
-
-        self.nft.token.transfer(&self.nft.owner, &msg::source(), self.nft.token_id);
-
+        self.is_using = false;
         let refund = msg::value() - price;
-        
-        msg::send(msg::source(), [], refund);
-        msg::send(self.nft.owner, [], price);
+
+        msg::send(
+            self.nft.contract_id,
+            nft_example_io::Action::Transfer { to: msg::source(), token_id: self.nft.token_id },
+            0
+        );
+        msg::send(msg::source(), "", refund);
+        msg::send(self.nft.owner, "", price);
     }
 
     fn token_price(&self) -> U256 {
@@ -77,6 +77,34 @@ impl Auction {
         let discount = self.discount_rate * time_elapsed;
 
         self.starting_price - discount
+    }
+
+    fn renew_contract(&mut self, config: CreateConfig) {
+        if self.is_using {
+            panic!("already in use")
+        }
+
+        if config.starting_price < config.discount_rate * DURATION {
+            panic!("starting price < min");
+        }
+
+        self.is_using = true;
+        self.start_at = block_timestamp();
+        self.expires_at = block_timestamp() + DURATION;
+        self.nft.token_id = config.token_id;
+        self.nft.contract_id = config.nft_contract_actor_id;
+        self.nft.owner = config.token_owner;
+        self.discount_rate = config.discount_rate;
+        self.starting_price = config.starting_price;
+
+        msg::reply(
+            Event::AuctionStarted {
+                token_owner: self.nft.owner,
+                price: self.starting_price,
+                token_id: self.nft.token_id,
+            },
+            0,
+        );
     }
 }
 
@@ -93,33 +121,19 @@ gstd::metadata! {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn init() {
-    let config: InitConfig = msg::load().expect("Unable to decode InitConfig");
-    debug!("Auction {:?}", config);
-
-    if config.starting_price < config.discount_rate * DURATION {
-        panic!("starting price < min");
-    }
-
-    CONTRACT
-        .nft
-        .token
-        .init(config.name, config.symbol, config.base_uri);
-    CONTRACT.start_at = block_timestamp();
-    CONTRACT.expires_at = block_timestamp() + DURATION;
-    CONTRACT.nft.owner = msg::source();
-    CONTRACT.nft.token_id = config.token_id;
-    CONTRACT.discount_rate = config.discount_rate;
-    CONTRACT.starting_price = config.starting_price;
-}
+pub unsafe extern "C" fn init() { }
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
     let action: Action = msg::load().expect("Could not load Action");
+
+    if block_timestamp() >= CONTRACT.expires_at {
+        CONTRACT.is_using = false
+    }
+
     match action {
-        Action::Buy => {
-            CONTRACT.buy();
-        }
+        Action::Buy => CONTRACT.buy(),
+        Action::Create(config) => CONTRACT.renew_contract(config),
     }
 }
 
