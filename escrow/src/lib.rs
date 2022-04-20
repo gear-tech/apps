642449a92c34d35cad1232cb4576f12fc16d89f7
Luis Alberto Ramirez Albino <lara@MacBook-Pro.local>
2022-04-20 12:39:18 -0500

    Add unit test and use async functions

--- escrow/src/lib.rs
+++ escrow/src/lib.rs
@@ -2,11 +2,12 @@
 
 //Includes only the minimal gstd components for this smart contract
 use gstd::{
-    msg::{self, CodecMessageFuture},
+    msg::{self},
     exec, 
     prelude::*, 
     ActorId
 };
+use ft_io::{FTAction, FTEvent};
 
 //// The available states during the escrow
 enum State {
@@ -24,7 +25,7 @@ impl Default for State {
 
 #[derive(Default)]
 struct Escrow {
-    program_id: ActorId,
+    ft_token_id: ActorId,
     //Current escrow State
     current_state: State,
     //Buyer address
@@ -35,42 +36,8 @@ struct Escrow {
     amount: u128,
 }
 
-#[derive(Debug, Decode, Encode, TypeInfo)]
-pub enum FTAction {
-    Mint(u128),
-    Burn(u128),
-    Transfer {
-        from: ActorId,
-        to: ActorId,
-        amount: u128,
-    },
-    Approve {
-        to: ActorId,
-        amount: u128,
-    },
-    TotalSupply,
-    BalanceOf(ActorId),
-}
-
-#[derive(Debug, Encode, Decode, TypeInfo)]
-pub enum FTEvent {
-    Transfer {
-        from: ActorId,
-        to: ActorId,
-        amount: u128,
-    },
-    Approve {
-        from: ActorId,
-        to: ActorId,
-        amount: u128,
-    },
-    TotalSupply(u128),
-    Balance(u128),
-}
-
-
-pub fn transfer_tokens(token_id: &ActorId, from: &ActorId, to: &ActorId, amount: u128) -> CodecMessageFuture<FTEvent> {
-    msg::send_and_wait_for_reply(
+pub async fn transfer_tokens(token_id: &ActorId, from: &ActorId, to: &ActorId, amount: u128) {
+    let _transfer_response: FTEvent = msg::send_and_wait_for_reply(
         *token_id,
         FTAction::Transfer {
             from: *from,
@@ -79,6 +46,26 @@ pub fn transfer_tokens(token_id: &ActorId, from: &ActorId, to: &ActorId, amount:
         },
         0,
     ).unwrap()
+    .await
+    .expect("Error in transfer");
+}
+
+#[derive(Debug, Decode, Encode)]
+pub enum EscrowActions {
+    Deposit(),
+    ConfirmDeliery(),
+}
+
+#[derive(Decode, Encode)]
+pub enum EscrowEvent {
+    Deposit {
+        buyer: ActorId,
+        amount: u128,
+    },
+    Confirm {
+        seller: ActorId,
+        amount: u128,
+    },
 }
 
 impl Escrow {
@@ -89,7 +76,7 @@ impl Escrow {
 
     //The buyer makes a deposit only when a payment is pending
     //and the seller receives the funds
-    fn deposit(&mut self) {
+    async fn deposit(&mut self) {
         if matches!(self.current_state, State::PendingPayment) {
             panic!("Already Payed");
         }
@@ -99,19 +86,28 @@ impl Escrow {
 
         //Sends the transfer from buyer to smart contract
         transfer_tokens(
-            &self.program_id,
+            &self.ft_token_id,
             &self.buyer,
             &exec::program_id(),
             self.amount,
-        );
+        ).await;
 
         self.current_state = State::PendingDelivery;
+
+        msg::reply(
+            EscrowEvent::Deposit {
+                buyer: self.buyer,
+                amount: self.amount,
+            },
+            0,
+        )
+        .unwrap();
     }
 
     //The buyer confirms the delivery only when the delivery is
     //pending, the balance is transfered to the seller
     //and  the  transaction is finished.
-    fn confirm_delivery(&mut self) {
+    async fn confirm_delivery(&mut self) {
         if matches!(self.current_state, State::PendingDelivery) {
             panic!("Cannot confirm delivery");
         }
@@ -121,37 +117,64 @@ impl Escrow {
 
         //Sends the transfer from buyer to seller
         transfer_tokens(
-            &self.program_id,
+            &self.ft_token_id,
             &exec::program_id(),
             &self.seller,
             self.amount,
-        );
+        ).await;
 
         self.current_state = State::Finished;
-    }
-}
 
-#[derive(Debug, Decode, Encode)]
-pub enum EscrowActions {
-    Deposit(),
-    ConfirmDeliery(),
+        msg::reply(
+            EscrowEvent::Confirm {
+                seller: self.seller,
+                amount: self.amount,
+            },
+            0,
+        )
+        .unwrap();
+    }
 }
 
 static mut ESCROW: Option<Escrow> = None;
 
 #[no_mangle]
-pub unsafe extern "C" fn handle() {
+pub async unsafe extern "C" fn handle() {
     let action: EscrowActions = msg::load().expect("Could not load Action");
-    let ft = ESCROW.get_or_insert(Default::default());
+    let escrow = ESCROW.get_or_insert(Default::default());
 
     match action {
         EscrowActions::Deposit() => {
-            ft.deposit();
+            escrow.deposit().await;
         }
         EscrowActions::ConfirmDeliery() => {
-            ft.confirm_delivery();
+            escrow.confirm_delivery().await;
         }
     }
 }
 
+#[cfg(test)]
+mod tests {
 
+    extern crate std;
+    use gtest::{Program, System};
+
+    pub const _FT: u64 = 2;
+    pub const _FOREIGN_USER: u64 = 2;
+    pub const _BUYER: [u64; 2] = [1, 2];
+    pub const _SELLER: [u64; 2] = [3, 4];
+    pub const _AMOUNT: [u128; 2] = [50, 200];
+    pub const _CONTRACT: [u128; 2] = [1, 2];
+
+    #[test]
+    fn wrong_buyer_deposit() {
+        let system = System::new();
+        let program = Program::current(&system);
+
+        system.init_logger();
+
+        assert!(program
+        .send(_FOREIGN_USER, super::EscrowActions::Deposit())
+        .main_failed());
+    }
+}
