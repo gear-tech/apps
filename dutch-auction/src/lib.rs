8502a42be5a8424c68ca0b4360b89bdf5fac437e
puzzle-rusher <puzzlerusher@gmail.com>
2022-04-23 21:39:27 +0400

    adding async min function

--- dutch-auction/src/lib.rs
+++ dutch-auction/src/lib.rs
@@ -14,14 +14,14 @@ pub use auction_io::{Action, Event, InitConfig, CreateConfig};
 const ZERO_ID: ActorId = ActorId::new([0u8; 32]);
 const DURATION: u64 = 7 * 24 * 60 * 60 * 1000;
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct NFT {
     pub token_id: U256,
     pub owner: ActorId,
     pub contract_id: ActorId,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct Auction {
     pub nft: NFT,
     pub starting_price: U256,
@@ -31,21 +31,10 @@ pub struct Auction {
     pub expires_at: u64,
 }
 
-static mut CONTRACT: Auction = Auction {
-    nft: NFT {
-        token_id: U256::zero(),
-        owner: ZERO_ID,
-        contract_id: ZERO_ID,
-    },
-    starting_price: U256::zero(),
-    discount_rate: U256::zero(),
-    is_active: false,
-    start_at: 0,
-    expires_at: 0,
-};
+static mut AUCTION: Option<Auction> = None;
 
 impl Auction {
-    fn buy(&mut self) {
+    async fn buy(&mut self) {
         if !self.is_active {
             panic!("already bought or auction expired");
         }
@@ -63,13 +52,17 @@ impl Auction {
         self.is_active = false;
         let refund = msg::value() - price;
 
-        msg::send_and_wait_for_reply(
+        let _transfer_response: nft_example_io::Event = msg::send_and_wait_for_reply(
             self.nft.contract_id,
             nft_example_io::Action::Transfer { to: msg::source(), token_id: self.nft.token_id },
             0
-        );
-        msg::send_and_wait_for_reply(msg::source(), "", refund);
-        msg::send_and_wait_for_reply(self.nft.owner, "", price);
+        )
+        .unwrap()
+        .await
+        .expect("Error in nft transfer");
+
+        msg::send(msg::source(), "", refund);
+        msg::send(self.nft.owner, "", price);
     }
 
     fn token_price(&self) -> U256 {
@@ -138,28 +131,30 @@ gstd::metadata! {
 #[no_mangle]
 pub unsafe extern "C" fn init() { }
 
-#[no_mangle]
-pub unsafe extern "C" fn handle() {
+#[gstd::async_main]
+async unsafe fn main() {
     let action: Action = msg::load().expect("Could not load Action");
+    let auction: &mut Auction = unsafe { AUCTION.get_or_insert(Auction::default()) };
 
-    CONTRACT.stop_if_time_is_over();
+    auction.stop_if_time_is_over();
 
     match action {
-        Action::Buy => CONTRACT.buy(),
-        Action::Create(config) => CONTRACT.renew_contract(config),
+        Action::Buy => auction.buy().await,
+        Action::Create(config) => auction.renew_contract(config),
     }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn meta_state() -> *mut [i32; 2] {
     let query: State = msg::load().expect("failed to decode input argument");
+    let auction: &mut Auction = unsafe { AUCTION.get_or_insert(Auction::default()) };
 
-    CONTRACT.stop_if_time_is_over();
+    auction.stop_if_time_is_over();
 
     let encoded = match query {
-        State::TokenPrice() => StateReply::TokenPrice(CONTRACT.token_price()),
-        State::IsActive() => StateReply::IsActive(CONTRACT.is_active),
-        State::Info() => StateReply::Info(CONTRACT.info()),
+        State::TokenPrice() => StateReply::TokenPrice(auction.token_price()),
+        State::IsActive() => StateReply::IsActive(auction.is_active),
+        State::Info() => StateReply::Info(auction.info()),
     }.encode();
     let result = gstd::macros::util::to_wasm_ptr(&(encoded[..]));
 
