7c295f1a42c312b33c17bac40079a4281308947a
Akado2009 <prosvirov.k@gmail.com>
2022-05-08 22:00:36 -0700

    Rewrite concert logic

--- concert/io/src/lib.rs
+++ concert/io/src/lib.rs
@@ -1,14 +1,23 @@
 #![no_std]
 
 use codec::{Decode, Encode};
-use gear_contract_libraries::erc1155::io::*;
 use gstd::{prelude::*, ActorId};
 use scale_info::TypeInfo;
+
+// This is afrom contract, remove later to imports :)
+pub type TokenId = u128;
+
+#[derive(Debug, Decode, Encode, TypeInfo, Default, Clone)]
+pub struct TokenMetadata {
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub media: Option<String>,
+    pub reference: Option<String>,
+}
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub enum ConcertAction {
     Create {
-        contract_id: ActorId,
         creator: ActorId,
         concert_id: u128,
         no_tickets: u128,
@@ -17,7 +26,7 @@ pub enum ConcertAction {
     Hold {
         concert_id: u128,
     },
-    BuyTicket {
+    BuyTickets {
         concert_id: u128,
         amount: u128,
         metadata: Vec<Option<TokenMetadata>>,
@@ -42,7 +51,6 @@ pub enum ConcertEvent {
 
 #[derive(Debug, Encode, Decode, TypeInfo)]
 pub struct InitConcert {
-    pub name: String,
-    pub symbol: String,
-    pub base_uri: String,
+    pub owner_id: ActorId,
+    pub mtk_contract: ActorId,
 }
--- concert/src/lib.rs
+++ concert/src/lib.rs
@@ -9,6 +9,7 @@ const NFT_COUNT: u128 = 1;
 
 #[derive(Debug, Default)]
 pub struct Concert {
+    pub owner_id: ActorId,
     pub contract_id: ActorId,
 
     pub name: u128,
@@ -22,96 +23,46 @@ pub struct Concert {
     pub metadata: BTreeMap<ActorId, BTreeMap<u128, Option<TokenMetadata>>>,
 }
 
-impl StateKeeper for Concert {
-    fn get(&self) -> &ERC1155State {
-        &self.tokens
-    }
-    fn get_mut(&mut self) -> &mut ERC1155State {
-        &mut self.tokens
-    }
-}
-
-
-pub trait ConcertCore: ERC1155Core {
-    // name -- concert ID to use as a token ID for fungible
-    // number_of -- number of tickets
-    fn create_concert(
-        &mut self,
-        contract_id: ActorId,
-        creator: ActorId,
-        concert_id: u128,
-        no_tickets: u128,
-    );
-
-    // who buys
-    // how many
-    fn buy_tickets(&mut self, concert_id: u128, amount: u128, metadata: Vec<Option<TokenMetadata>>);
-
-    fn hold_concert(&mut self, concert_id: u128);
-
-    fn proc(&mut self, bytes: Vec<u8>) -> Option<()> {
-        if bytes.len() < 2 {
-            return None;
-        }
-        let action = ConcertAction::decode(&mut &bytes[..]).ok()?;
-        // debug!("action = {:?}", action);
-        match action {
-            ConcertAction::Create {
-                contract_id,
-                creator,
-                concert_id,
-                no_tickets,
-            } => <Self as ConcertCore>::create_concert(
-                self,
-                contract_id,
-                creator,
-                concert_id,
-                no_tickets,
-            ),
-            ConcertAction::BuyTicket {
-                concert_id,
-                amount,
-                metadata,
-            } => <Self as ConcertCore>::buy_tickets(self, concert_id, amount, metadata),
-            ConcertAction::Hold { concert_id } => {
-                <Self as ConcertCore>::hold_concert(self, concert_id);
-            }
-        }
-        Some(())
-    }
-}
-
 static mut CONTRACT: Option<Concert> = None;
 
 #[no_mangle]
 pub unsafe extern "C" fn init() {
     let config: InitConcert = msg::load().expect("Unable to decode InitConfig");
     let mut concert = Concert::default();
-    concert.tokens.name = config.name;
-    concert.tokens.symbol = config.symbol;
-    concert.tokens.base_uri = config.base_uri;
+    concert.owner_id = config.owner_id;
+    concert.contract_id = config.mtk_contract;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn handle() {
-    let action: Vec<u8> = msg::load().expect("Could not load msg");
-    let concert = CONTRACT.get_or_insert(Concert::default());
-    ConcertCore::proc(concert, action);
+#[gstd::async_main]
+async unsafe fn main() {
+    let action: ConcertAction = msg::load().expect("Could not load Action");
+    let concert: &mut Concert = unsafe {CONTRACT.get_or_insert(Concert::default()) };
+    match action {
+        ConcertAction::Create {
+            creator,
+            concert_id,
+            no_tickets,
+        } => {
+            concert.create_concert(creator, concert_id, no_tickets)
+        }
+        ConcertAction::Hold {
+            concert_id,
+        } => {
+            concert.hold_concert(concert_id).await
+        }
+        ConcertAction::BuyTickets {
+            concert_id,
+            amount,
+            metadata,
+        } => {
+            concert.buy_tickets(concert_id, amount, metadata).await
+        }
+    }
 }
 
-// #[no_mangle]
-// pub unsafe extern "C" fn meta_state() -> *mut [i32; 2] {
-//     let query: Vec<u8> = msg::load().expect("failed to decode input argument");
-//     let concert = CONTRACT.get_or_insert(Concert::default());
-//     // let encoded = ERC1155Core::proc_state(concert, query).expect("error");
-//     let encoded: Option<Vec<u8>>= None;
-//     gstd::util::to_leak_ptr(encoded)
-// }
-
-impl ConcertCore for Concert {
+impl Concert {
     fn create_concert(
         &mut self,
-        contract_id: ActorId,
         creator: ActorId,
         concert_id: u128,
         number_of_tickets: u128,
@@ -120,7 +71,6 @@ impl ConcertCore for Concert {
         self.id_counter = concert_id;
         self.name = concert_id;
         self.number_of_tickets = number_of_tickets;
-        self.contract_id = contract_id;
         msg::reply(
             ConcertEvent::Creation {
                 creator,
@@ -132,7 +82,7 @@ impl ConcertCore for Concert {
         .unwrap();
     }
 
-    fn buy_tickets(&mut self, concert_id: u128, amount: u128, mtd: Vec<Option<TokenMetadata>>) {
+    async fn buy_tickets(&mut self, concert_id: u128, amount: u128, mtd: Vec<Option<TokenMetadata>>) {
         if msg::source() == ZERO_ID {
             panic!("CONCERT: Message from zero address");
         }
@@ -159,6 +109,7 @@ impl ConcertCore for Concert {
 
         self.buyers.insert(msg::source());
 
+        // !TODO: Actions from MTK.
         msg::send_and_wait_for_reply(
             self.contract_id,
             ERC1155Action::Mint(&msg::source(), &concert_id, amount, None),
@@ -172,16 +123,17 @@ impl ConcertCore for Concert {
     }
 
     // MINT SEVERAL FOR A USER
-    fn hold_concert(&mut self, concert_id: u128) {
+    async fn hold_concert(&mut self, concert_id: u128) {
         if msg::source() != self.creator {
             panic!("CONCERT: Only creator can hold a concert");
         }
         // get balances from a contract
         let accounts: Vec<_> = self.buyers.into_iter().collect();
         let tokens: Vec<TokenId> = iter::repeat(self.name).take(accounts.len()).collect();
+        // !TODO: Actions from MTK.
         let balances = msg::send_and_wait_for_reply(
             self.contract_id,
-            ERC1155Action::BalanceOfBatch(tokens, balances),
+            ERC1155Action::BalanceOfBatch(tokens, accounts),
             0,
         )
         .unwrap()
@@ -212,6 +164,7 @@ impl ConcertCore for Concert {
                     meta.push(token_meta);
                 }
 
+                // !TODO: Actions from MTK.
                 msg::send_and_wait_for_reply(
                     self.contract_id,
                     ERC1155Action::MintBatch(actor, &ids, &amounts, meta),
