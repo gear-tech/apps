ffa11ab60ca1ca0e290fbd9e743674772e6c2395
Akado2009 <prosvirov.k@gmail.com>
2022-05-17 21:58:30 -0700

    Add transfer comments

--- rmrk/src/approvals.rs
+++ rmrk/src/approvals.rs
@@ -2,6 +2,20 @@ use crate::*;
 use gstd::{debug, msg, ActorId};
 
 impl RMRKToken {
+    /// That function is designed to be from another RMRK contracts
+    /// when transfering tokens to a non-nft address
+    /// It checks for the ownership and whether the msg::source is an NFT contract
+    /// if it is it burns itself from root_owner
+    /// then it just swaps the balances
+    /// Requirements:
+    ///
+    /// * The `msg::source()` must be a deployed RMRK contract
+    /// * The ``token_id` should exist
+    /// * The `to` address should be a non-zero address
+    /// * The `msg::source()` must be approved or owner of the token
+    /// Arguments:
+    /// * `to`: is the receiving address
+    /// * `token_id`: is the tokenId of the transfered token
     pub async fn transfer(&mut self, to: &ActorId, token_id: TokenId) {
         let rmrk_owner = self
             .rmrk_owners
@@ -19,6 +33,23 @@ impl RMRKToken {
         msg::reply(RMRKEvent::Transfer { to: *to, token_id }, 0).unwrap();
     }
 
+
+    /// That function is designed to be from another RMRK contracts
+    /// when transfering tokens to a non-nft address
+    /// It checks for the ownership and whether the msg::source is an NFT contract
+    /// if it is it burns itself from root_owner
+    /// then it just swaps the balances
+    /// If to is also an NFT contract it adds a child to destination_id token
+    /// Requirements:
+    ///
+    /// * The `msg::source()` must be a deployed RMRK contract
+    /// * The ``token_id` should exist
+    /// *
+    /// * The `to` address should be a non-zero address
+    /// * The `msg::source()` must be approved or owner of the token
+    /// Arguments:
+    /// * `to`: is the receiving address
+    /// * `token_id`: is the tokenId of the transfered token
     pub async fn transfer_to_nft(
         &mut self,
         to: &ActorId,
@@ -59,81 +90,8 @@ impl RMRKToken {
                 owner_id: *to,
             },
         );
-        debug!("TO: {:?}", to);
-        debug!("TOKEN_ID: {:?}", token_id);
         msg::reply(RMRKEvent::Transfer { to: *to, token_id }, 0).unwrap();
     }
-
-    // pub async fn _transfer(
-    //     &mut self,
-    //     to: &ActorId,
-    //     _from: &ActorId,
-    //     token_id: TokenId,
-    //     to_token_id: TokenId,
-    // ) {
-    //     self.assert_zero_address(to);
-    //     self.assert_approved_or_owner(token_id);
-    //     let rmrk_owner = self
-    //         .rmrk_owners
-    //         .get(&token_id)
-    //         .expect("Token does not exist");
-
-    //     self.balances
-    //         .entry(rmrk_owner.owner_id)
-    //         .and_modify(|balance| *balance -= 1);
-    //     let mut ch_ids: Vec<TokenId> = Vec::new();
-    //     let mut ch_token_ids: Vec<ActorId> = Vec::new();
-    //     let mut ch_statuses: Vec<ChildStatus> = Vec::new();
-    //     // // our owner is an nft also
-    //     if rmrk_owner.token_id.is_some() {
-    //         let parent_contract = rmrk_owner.owner_id;
-    //         // transfer all the children
-    //         if let Some(ch_map) = self.children.get(&token_id) {
-    //             for (child_token_id, child) in ch_map.clone().iter() {
-    //                 ch_ids.push(*child_token_id);
-    //                 ch_token_ids.push(child.token_id);
-    //                 ch_statuses.push(child.status);
-    //             }
-    //             // transfer children with add = false, since we want to remove those
-    //             transfer_children(
-    //                 &parent_contract,
-    //                 token_id,
-    //                 ch_ids.clone(),
-    //                 ch_token_ids.clone(),
-    //                 ch_statuses.clone(),
-    //                 false,
-    //             )
-    //             .await;
-    //         }
-    //     }
-
-    //     let rmk_dest = self
-    //         .rmrk_owners
-    //         .get(&to_token_id)
-    //         .expect("Token does not exist");
-    //     // the destination is an nft
-    //     if rmk_dest.token_id.is_some() {
-    //         // get nextOwner
-    //         let next_owner = get_root_owner(&rmk_dest.owner_id, to_token_id).await;
-    //         self.balances
-    //             .entry(next_owner)
-    //             .and_modify(|balance| *balance += 1);
-
-    //         transfer_children(
-    //             &next_owner,
-    //             to_token_id,
-    //             ch_ids.clone(),
-    //             ch_token_ids.clone(),
-    //             ch_statuses.clone(),
-    //             true,
-    //         )
-    //         .await;
-    //     } else {
-    //         self.balances.entry(*to).and_modify(|balance| *balance += 1);
-    //     }
-
-    //     msg::reply(RMRKEvent::Transfer { to: *to, token_id }.encode(), 0).unwrap();
-    // }
 
     pub async fn approve(&mut self, to: &ActorId, token_id: TokenId) {
         let owner = self.assert_owner(token_id).await;
